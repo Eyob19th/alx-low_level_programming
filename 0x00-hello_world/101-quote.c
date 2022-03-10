594e3a7abe6b95384af0b0a5d162d577a33fc253
Eyob19th <getacheweyob732@gmail.com>
2022-03-10 14:43:35 -0800

    commited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
   * Return: 1 (Failure)
   */
 int main(void)
-{
-	fprintf(stderr, "%s",
-			"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+{ 
+	fprintf(stderr, "%s,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
