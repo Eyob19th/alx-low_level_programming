dcdea7b1e4449e79a255866bc0cbb28e8f7651d7
Eyob19th <getacheweyob732@gmail.com>
2022-03-10 11:31:40 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  * main the first where compiling point starts
+  *
+  *return : Always 0 (success)
+  */
+int main(void)
+{
+	char str1[60];
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	puts(str1);
+	return (0);
+}
