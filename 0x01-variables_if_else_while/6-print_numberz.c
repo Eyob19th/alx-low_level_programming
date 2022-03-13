a6189723c93d61b462b1125c82b74bcabee74648
Eyob19th <getacheweyob732@gmail.com>
2022-03-13 14:40:15 -0700

    commited

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdilb.h>
+#include <string.h>
+/**
+  *main - entry point
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c)
+	}
+	putchar('\n');
+	return (0);
+}#include <stdio.h>
+#include <stdilb.h>
+#include <string.h>
+/**
+  *main - entry point
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c)
+	}
+	putchar('\n');
+	return (0);
+}
+#include <stdio.h>
+#include <stdilb.h>
+#include <string.h>
+/**
+  *main - entry point
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c)
+	}
+	putchar('\n');
+	return (0);
+}
