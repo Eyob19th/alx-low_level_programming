f375fdfeb59bf82b6868e1a0dff59bead270664b
Eyob19th <getacheweyob732@gmail.com>
2022-03-15 13:51:46 -0700

    commited

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *print_alphabet - prints alphabet in lower case
+  *
+  *Description: print alphabets
+  */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+  *print_alphabet_x10 - repeats the print_alphabet 10 times.
+  *Return: Nothing.
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (1 - 0; 1 < 10; ++1)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
