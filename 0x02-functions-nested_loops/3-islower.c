ddd1afbef9f5b0a81458149e91dd7259824a1c51
Eyob19th <getacheweyob732@gmail.com>
2022-03-16 15:00:02 -0700

    commited

---
+++ 0x02-functions-nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_islower - check whetre the input is lowercase or not
+  *@c: char type c
+  *Return: 1 (lowercase) and 0 (uppercase)
+  */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
