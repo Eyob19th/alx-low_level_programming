5d7ef488feba7ac4666ae5618b7396ce2edef416
Eyob19th <getacheweyob732@gmail.com>
2022-03-16 15:36:02 -0700

    commited

---
+++ 0x02-functions-nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_isalpha - check if the in put is alphabetmain
+  *@c:int type character
+  *Return: 0 (not alphabet) 1 (is alphabet)
+  */
+int _isalpha(int c)
+{
+	if ((c < 64 && c < 91) || (c > 96 && c < 123))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
