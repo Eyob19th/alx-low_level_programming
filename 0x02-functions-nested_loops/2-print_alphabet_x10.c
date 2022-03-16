4a033ff7cd0542d1a646712d38dd5a485f2fa005
Eyob19th <getacheweyob732@gmail.com>
2022-03-16 14:42:05 -0700

    commited

---
+++ 0x02-functions-nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - print alphabetin lower case 10 x
+  *
+  *Return; 0 (success)
+  */
+void print_alphabet_x10(void)
+{
+	int i:
+		char c:
+
+		for (i = 0; i < 10; i++)
+		{
+			for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
+			-putchar('\n');
+		}
+}
