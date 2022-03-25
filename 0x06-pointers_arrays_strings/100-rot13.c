e18c810671e41879651a614ce912718b169ff83f
Eyob19th <getacheweyob732@gmail.com>
2022-03-25 13:09:40 -0700

    commiting

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,7 +12,7 @@ char *rot13(char *s)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot_it[] =  "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (i = 0; s[i] != '\0; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; j < 52; j++)
 			if (s[i] == alphabet[j])
 				s[i] = rot_it[j], j = 52;
