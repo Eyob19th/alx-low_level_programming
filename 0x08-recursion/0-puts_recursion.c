42d7ee97f3bb498abe2c88331e1f20e3db92e0a5
Eyob19th <getacheweyob732@gmail.com>
2022-03-31 02:54:19 -0700

    commiting my 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+  *_puts_recursion - print a string followed by a new line
+  *@s: string
+  *
+  *Return: on success 1
+  *on error, -1 is returned, and errno is set appropriately
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
