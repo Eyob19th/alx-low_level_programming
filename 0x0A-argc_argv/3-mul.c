0cc55aedb0b6f7ae6f3ff02f288e857a2dae6db0
Eyob19th <getacheweyob732@gmail.com>
2022-04-02 04:01:44 -0700

    argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,3 +13,16 @@ int main(int argc, char **argv)
 	int n, ex;
 
 	ex = 0;
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		ex = 1;
+	}
+	else
+	{
+		n = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", n);
+	}
+
+	return (ex);
+}
